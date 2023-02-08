c5d9a40872833dbba692368cb0622a3bc8bead87
rashafikov <rashafikov@gmail.com>
2023-02-08 14:22:30 +0300

    Задача 24

---
+++ Задача 24/Program.cs
@@ -0,0 +1,11 @@
+﻿// Подсчитать сумму цифр в числе
+Console.Clear();
+Console.WriteLine("Введите число");
+int chislo = int.Parse(Console.ReadLine()!);
+int result = 0;
+while (chislo != 0)
+{
+    result += chislo % 10;
+    chislo /= 10;
+}
+Console.WriteLine($"Сумма цифр введенного числа = {Math.Abs(result)}");
