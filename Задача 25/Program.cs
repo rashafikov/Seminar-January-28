789316d5d614f80b5a211d5c0dd7e7a649f38eb0
rashafikov <rashafikov@gmail.com>
2023-02-08 14:29:31 +0300

    Задача 25

---
+++ Задача 25/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.Clear();
+Console.WriteLine("Введите число");
+int chislo = int.Parse(Console.ReadLine()!);
+int result = 1;
+for (int i = 1; i < chislo; i++)
+{
+    result *= i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {chislo} = {result}");
