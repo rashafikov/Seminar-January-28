649b12a6bf2976b671fb1e595519955bad1256e1
rashafikov <rashafikov@gmail.com>
2023-02-08 14:33:43 +0300

    Задача 26

---
+++ Задача 26/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+Console.Clear();
+Console.WriteLine("Введите число");
+int chislo = int.Parse(Console.ReadLine()!);
+int result = 0;
+for (int i = 0; i < chislo; i++)
+{
+result = i*i*i;
+if (result % 2 == 0) Console.WriteLine(result);
+}
